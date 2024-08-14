91f3799ed18112ffd538339177cb9295ced0a9a8
AZRAELmd <kingazrael44@gmail.com>
2024-08-14 18:22:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK0d3dSt2UDRUSE8xcHN3UGo5TEdHeWtvYndEZ1pNa0YvaDNOdTlEK28zYz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSmMyR2xwZG11RlVUWTkwcEVQaGFqSVBYYlcwYmE5cHFMeWtIb1RNT0ZSOD0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJlTnJqd2FDMCsvUDlUUDVieHE3bDdnV0FBaUpoRXpUazlRekpDeXJERGxrPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJubU9rSzV2elA5SHhyUWVjWkNiOFlGTkM0cmFRZGlFbHp2Tko2aXNzblFnPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InFFekw5RGVkME1tSURuZ2VlUEQ2cDJHTjNDTXBPeXZXV1VkYjVSb0dNVWs9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjlycDdJRVVNa2duaXk4WEU2K2Y0L084WERVcmNoTUJWWXA1djhrZEx3RkU9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib09Uc3NjMy82dmRoeDhqRmR5QkZjMzdZTEhyMGRqaUlNY3RBNXhlKzkxcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiUDVKd0dUNFJUN0FUeWZ1M29IOFdjQjJYMFAvQjNaa21LZGhSRjgxMkhURT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Imp0VlNUbDc4WStEV2N1a2VEdG8xamE1UDc2VFVrNUZja0pXTVRVRVowem5rbmtSbldKSy9RZEwxR2N3cnIyVVczb0lUd0JJZjNycEYvV3lQT041QmlRPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTM3LCJhZHZTZWNyZXRLZXkiOiJLclVydnpINjM2WFlCRXArdzNWN25WWTBLNVlsdnJ3OUV0R1NHMHF6ZlpzPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiIyc2wxN2hhNFRaZW5FTllfS3g2a0VBIiwicGhvbmVJZCI6ImViNmZhNzkzLTkyNTktNDMxYi1iODE4LTg0MTBiMDAxNzU1YyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJaeTBwOCtLYlQ1VC9OaGpnekFBQ2RoRnhHQlE9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOGl5K0xEbnZMNkVkY1UrTGZWM29EOFhOdGlvPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlJDNExSUlAyIiwibWUiOnsiaWQiOiIyMzc2NTkyNjI2NTM6MkBzLndoYXRzYXBwLm5ldCIsIm5hbWUiOiLwnZCB8J2QpfCdkIDwnZCC8J2QiiDwnZCK8J2QiPCdkI3wnZCGIPCdkIvwnZCE8J2QjvCdkI3wnZCI8J2Qg/CdkIDwnZCSIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNQWHF4VThRNU1uenRRWVlBaUFBS0FBPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJBRVliSzgrWCtUMmpCR0prNUdwUko4dExOZGFZa3BUbWlEMTA3Q01ZTEVRPSIsImFjY291bnRTaWduYXR1cmUiOiI5VEhPMlhEa2xvcVNkZXpIZWw1UXNGb3BtZEZhYm1oWHFHZWRGNHZ2NHR2S09TR01hODlRRWdOdkhtai9PNlBkTXFLSmFwRU50SUhGelBjLzR1NHpBUT09IiwiZGV2aWNlU2lnbmF0dXJlIjoicWZqWVNzRDBudDQ0R1I3WVoyeGRmTXBNTFZZR21nM0o3Q1JnUjc1dFBpL0d3NEJDdzVoQVNrUk1ML3RtT3NaV2U2TEk0cnFsNUQ4cWhRWm82NnBVanc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiIyMzc2NTkyNjI2NTM6MkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJRQkdHeXZQbC9rOW93UmlaT1JxVVNmTFN6WFdtSktVNW9nOWRPd2pHQ3hFIn19XSwicGxhdGZvcm0iOiJzbWJhIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIzNjU1NDExLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQU0vaSJ9";
 global.MONGODB = process.env.MONGODB_URI || "";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
@@ -12,7 +12,7 @@ global.sudo = process.env.SUDO
   : "null";
 global.owner = process.env.OWNER_NUMBER
   ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
-  : "null";
+  : "659262653";
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
   process.env.IMAGE ||
@@ -24,14 +24,14 @@ global.userImages =
 
 module.exports = {
   menu: process.env.MENU || "",
-  HANDLERS: process.env.PREFIX || "",
+  HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "`©ᴠᴇʟᴅʀᴀ-ᴍᴅ`",
-  author: process.env.PACK_AUTHER || "𝐕𝐞𝐥𝐝𝐫𝐚-𝐌𝐝",
-  packname: process.env.PACK_NAME || "𝐋𝐢𝐦𝐮𝐥𝐞 𝐒𝐨𝐥𝐢𝐭𝐚𝐫𝐮𝐬",
-  botname: process.env.BOT_NAME || "ᴠᴇʟᴅʀᴀ-ᴍᴅ",
-  ownername: process.env.OWNER_NAME || "Limule Solitarus",
+  author: process.env.PACK_AUTHER || "𝐁𝐥𝐀𝐂𝐊 𝐊𝐈𝐍𝐆 𝐋𝐄𝐎𝐍𝐈𝐃𝐀𝐒",
+  packname: process.env.PACK_NAME || "𝐁𝐥𝐀𝐂𝐊 𝐊𝐈𝐍𝐆 𝐋𝐄𝐎𝐍𝐈𝐃𝐀𝐒",
+  botname: process.env.BOT_NAME || "𝐁𝐥𝐀𝐂𝐊 𝐊𝐈𝐍𝐆 𝐋𝐄𝐎𝐍𝐈𝐃𝐀𝐒",
+  ownername: process.env.OWNER_NAME || "𝐁𝐥𝐀𝐂𝐊 𝐊𝐈𝐍𝐆 𝐋𝐄𝐎𝐍𝐈𝐃𝐀𝐒",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
